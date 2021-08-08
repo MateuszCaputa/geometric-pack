280fa53a5396c8fb7a08093dac74a4c4b2706991
Mateusz Caputa <caputa.mateusz.pl@gmail.com>
2021-08-09 00:51:51 +0200

    fix(QuadraticFunction): bug fix where in test, object of positive/negative range is possibly undefind despite it is never a case

--- src/functions/quadratic-function/quadratic-function.test.ts
+++ src/functions/quadratic-function/quadratic-function.test.ts
@@ -1,3 +1,4 @@
+import { QuadraticRange } from "./models/quadratic-range";
 import { QuadraticFunction } from "./quadratic-function";
 
 const firstQuadraticFunction = new QuadraticFunction(-4, 4, 1);
@@ -76,37 +77,55 @@ describe("QuadraticFunction", () => {
   });
 
   test("should get start point of positive range", () => {
-    const result = firstQuadraticFunction.getPositiveRange()[0][0];
+    const data = firstQuadraticFunction.getPositiveRange() as [QuadraticRange];
+    const result = data[0][0];
     const expected = -0.20710678118654757;
     expect(result).toBe(expected);
   });
 
   test("should get end point of positive range", () => {
-    const result = firstQuadraticFunction.getPositiveRange()[0][1];
+    const data = firstQuadraticFunction.getPositiveRange() as [QuadraticRange];
+    const result = data[0][1];
     const expected = 1.2071067811865475;
     expect(result).toBe(expected);
   });
 
   test("should get start point of first negative range", () => {
-    const result = firstQuadraticFunction.getNegativeRange()[0][0];
+    const data = firstQuadraticFunction.getNegativeRange() as [
+      QuadraticRange,
+      QuadraticRange
+    ];
+    const result = data[0][0];
     const expected = -Infinity;
     expect(result).toBe(expected);
   });
 
   test("should get end point of first negative range", () => {
-    const result = firstQuadraticFunction.getNegativeRange()[0][1];
+    const data = firstQuadraticFunction.getNegativeRange() as [
+      QuadraticRange,
+      QuadraticRange
+    ];
+    const result = data[0][1];
     const expected = -0.20710678118654757;
     expect(result).toBe(expected);
   });
 
   test("should get start point of second negative range", () => {
-    const result = firstQuadraticFunction.getNegativeRange()[1][0];
+    const data = firstQuadraticFunction.getNegativeRange() as [
+      QuadraticRange,
+      QuadraticRange
+    ];
+    const result = data[1][0];
     const expected = 1.2071067811865475;
     expect(result).toBe(expected);
   });
 
   test("should get end point of second negative range", () => {
-    const result = firstQuadraticFunction.getNegativeRange()[1][1];
+    const data = firstQuadraticFunction.getNegativeRange() as [
+      QuadraticRange,
+      QuadraticRange
+    ];
+    const result = data[1][1];
     const expected = Infinity;
     expect(result).toBe(expected);
   });
